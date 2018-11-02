de417cc8a02d111097cda28ab25a772a9e181510
Sourabh Jajoria <sourabhjajoria@urbanclap.com>
2018-11-02 18:27:29 +0530

    Code to measure brightness, sharpness, size of an image. Sample images added.

---
+++ image_exposure.py
@@ -0,0 +1,15 @@
+import sys
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+    image_name = "test14.jpeg"
+    source_image = cv2.imread(image_name)
+    frame_hsv = cv2.cvtColor(source_image, cv2.COLOR_BGR2HSV)
+    channels = cv2.split(frame_hsv)
+    brightness = np.var(channels[2])
+    # Can take decision based on brightness
+    sharpness = cv2.Laplacian(source_image, cv2.CV_64F).var()
+    # Can take decision based on sharpness
+    image_size = np.size(source_image)
+    # Can take decision based on image_size
